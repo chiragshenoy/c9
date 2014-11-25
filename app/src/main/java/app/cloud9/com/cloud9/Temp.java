fd8578a93211a3926b52deba1e83010d8f2d94ab
chiragshenoy <chiragshenoy@gmail.com>
2014-11-26 00:36:14 +0530

    Remade tabs

--- app/src/main/java/app/cloud9/com/cloud9/Temp.java
+++ app/src/main/java/app/cloud9/com/cloud9/Temp.java
@@ -2,26 +2,45 @@ package app.cloud9.com.cloud9;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.util.TypedValue;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Temp extends ActionBarActivity {
-    ViewPager Tab;
-    TabPageAdapter TabAdapter;
+
     ActionBar actionBar;
 
-    TextView tv;
-    String s = "";
+    //    String s = "";
     ArrayList<String> arr;
     String all_marks_string;
 
     Bundle b;
+
+    private final Handler handler = new Handler();
+
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
+    private MyPagerAdapter adapter;
+
+    private Drawable oldBackground = null;
+    private int currentColor = 0xFF009688;
+
+    final Integer[] accent_color = {0x1DE9B6, 0x000000};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,72 +48,64 @@ public class Temp extends ActionBarActivity {
         setContentView(R.layout.workshops);
         Bundle bundle = getIntent().getExtras();
         all_marks_string = bundle.getString("marks");
-
-        b = new Bundle();
-        b.putString("all_marks", all_marks_string);
-
-        ArrayList<String> color = null;
-        final String[] color_list = {"#009688", "#FF5722", "#673AB7", "#00BCD4", "#CDDC39", "#FFC107", "#9E9E9E"};
-        final Random r = new Random();
-        r.nextInt();
-
         Bundle c = getIntent().getExtras();
+
+        final String[] color_list = {"#009688", "#FF5722", "#673AB7", "#00BCD4", "#CDDC39", "#FFC107", "#9E9E9E"};
+
 
         if (c != null) {
             arr = (ArrayList<String>) c.getStringArrayList("array_list");
         }
+        //
+
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setBackground(new ColorDrawable(Color.parseColor(color_list[0])));
+        tabs.setIndicatorColor(0xFFFFFFFF);
+        tabs.setTextColor(0xAAFFFFFF);
+
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
+
+        tabs.setViewPager(pager);
+        tabs.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                int color = Color.parseColor(color_list[i]);
+                changeColor(color);
+                tabs.setBackground(new ColorDrawable(Color.parseColor(color_list[i])));
+                // change_indicator_color(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        changeColor(currentColor);
+
+        b = new Bundle();
+        b.putString("all_marks", all_marks_string);
+
+        final Random r = new Random();
+        r.nextInt();
 
 
         Subject1 fragobj = new Subject1();
         fragobj.setArguments(b);
 
-        TabAdapter = new TabPageAdapter(getSupportFragmentManager());
-        TabAdapter.get_number_of_subject(arr.size());
-
-        Tab = (ViewPager) findViewById(R.id.pager);
-
-        Tab.setOnPageChangeListener(
-                new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        //actionBar = getSupportActionBar();
-                        getSupportActionBar().setSelectedNavigationItem(position);
-                        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(color_list[position])));
-                    }
-
-                    @Override
-                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                        super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-
-                    }
-                });
-        Tab.setAdapter(TabAdapter);
-
-
         actionBar = getSupportActionBar();
-//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor(a)));
-
-        //actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#9C27B0")));
-        //Enable Tabs on Action Bar
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-
-
-            @Override
-            public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-                Tab.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-
-            }
-
-            @Override
-            public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
-
-            }
-        };
 
 
         Bundle b = getIntent().getExtras();
@@ -103,24 +114,8 @@ public class Temp extends ActionBarActivity {
             arr = (ArrayList<String>) b.getStringArrayList("array_list");
         }
 
-        for (int i = 0; i < arr.size(); i++)
-            s += arr.get(i) + "";
-
-
-        for (int i = 0; i < arr.size(); i++) {
-            actionBar.addTab(actionBar.newTab().setText("" + arr.get(i)).setTabListener(tabListener));
-
-        }
-
     }
 
-    static int blendColors(int from, int to, float ratio) {
-        final float inverseRation = 1f - ratio;
-        final float r = Color.red(from) * ratio + Color.red(to) * inverseRation;
-        final float g = Color.green(from) * ratio + Color.green(to) * inverseRation;
-        final float b = Color.blue(from) * ratio + Color.blue(to) * inverseRation;
-        return Color.rgb((int) r, (int) g, (int) b);
-    }
 
     public String getAllMarks() {
         return all_marks_string;
@@ -133,4 +128,117 @@ public class Temp extends ActionBarActivity {
 
     }
 
+    public void change_indicator_color(int i) {
+        tabs.setIndicatorColor(accent_color[i]);
+    }
+
+    private void changeColor(int newColor) {
+
+        // change ActionBar color just if an ActionBar is available
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            Drawable colorDrawable = new ColorDrawable(newColor);
+            Drawable bottomDrawable = getResources().getDrawable(R.drawable.actionbar_bottom);
+            LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
+
+            if (oldBackground == null) {
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    ld.setCallback(drawableCallback);
+                } else {
+                    getSupportActionBar().setBackgroundDrawable(ld);
+                }
+
+            } else {
+
+                TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
+
+                // workaround for broken ActionBarContainer drawable handling on
+                // pre-API 17 builds
+                // https://github.com/android/platform_frameworks_base/commit/a7cc06d82e45918c37429a59b14545c6a57db4e4
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    td.setCallback(drawableCallback);
+                } else {
+                    getSupportActionBar().setBackgroundDrawable(td);
+                }
+
+                td.startTransition(200);
+
+            }
+
+            oldBackground = ld;
+
+            // http://stackoverflow.com/questions/11002691/actionbar-setbackgrounddrawable-nulling-background-from-thread-handler
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        }
+
+        currentColor = newColor;
+
+    }
+
+    private Drawable.Callback drawableCallback = new Drawable.Callback() {
+        @Override
+        public void invalidateDrawable(Drawable who) {
+            getSupportActionBar().setBackgroundDrawable(who);
+        }
+
+        @Override
+        public void scheduleDrawable(Drawable who, Runnable what, long when) {
+            handler.postAtTime(what, when);
+        }
+
+        @Override
+        public void unscheduleDrawable(Drawable who, Runnable what) {
+            handler.removeCallbacks(what);
+        }
+    };
+
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return arr.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return arr.size();
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case 0:
+                    return new Subject1();
+                case 1:
+                    return new Subject2();
+                case 2:
+                    return new Subject3();
+                case 3:
+                    return new Subject4();
+                case 4:
+                    return new Subject5();
+                case 5:
+                    return new Subject6();
+                case 6:
+                    return new Subject7();
+                case 7:
+                    return new Subject8();
+                case 8:
+                    return new Subject9();
+                case 9:
+                    return new Subject10();
+
+            }
+            return null;
+        }
+
+    }
 }
