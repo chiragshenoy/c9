b69d8bd1e5351c3e73874810d25d84e584ff656d
shubhamsizzles <shubhamsizzles@gmail.com>
2015-01-06 20:05:14 +0530

    Notice Board Revamp | Lots of other changes
    Added Search,
    Swipe to dismiss,
    Empty View
    etc.

--- app/src/main/java/app/cloud9/com/cloud9/NoticeBoard.java
+++ app/src/main/java/app/cloud9/com/cloud9/NoticeBoard.java
@@ -15,31 +15,45 @@ package app.cloud9.com.cloud9;
  * limitations under the License.
  */
 
-        import android.content.Context;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-        import android.support.v7.widget.Toolbar;
-        import android.view.GestureDetector;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoticeBoard extends ActionBarActivity {
+public class NoticeBoard extends ActionBarActivity implements SearchView.OnQueryTextListener{
 
-    private static final int ITEMS_COUNT = 20;
+    private static final int ITEMS_COUNT = 5;
     private List<String> mItems;
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mRecentRecyclerView;
+    private RecyclerView mOldRecyclerView;
+    private RecyclerView.LayoutManager mRecentLayoutManager;
+    private RecyclerView.LayoutManager mOldLayoutManager;
     private RecyclerView.Adapter<CustomViewHolder> mAdapter;
+    private SearchView mSearchView;
+    private Menu mMenu;
+    private RelativeLayout emptyNotice;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +61,31 @@ public class NoticeBoard extends ActionBarActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.c9_toolbar); //Appcompat support for a sexier action bar
         toolbar.setNavigationIcon(R.drawable.ic_drawer);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle(R.string.noticeboard_title);
         setSupportActionBar(toolbar);
         toolbar.setTitle(R.string.noticeboard_title);
+        toolbar.setNavigationIcon(R.drawable.ic_drawer);
+
+        emptyNotice = (RelativeLayout) findViewById(R.id.rl_empty_notice);
+        emptyNotice.setVisibility(View.GONE);
 
 
         initData();
         initRecyclerView();
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
     }
 
     private void initData() {
@@ -63,9 +96,17 @@ public class NoticeBoard extends ActionBarActivity {
     }
 
     private void initRecyclerView() {
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecentRecyclerView = (RecyclerView) findViewById(R.id.recentrecyclerView);
+        mRecentRecyclerView.setHasFixedSize(true);
+//        mOldRecyclerView = (RecyclerView) findViewById(R.id.oldrecyclerView);
+
+        mRecentLayoutManager = new LinearLayoutManager(this);
+//        mOldLayoutManager = new LinearLayoutManager(this);
+
+
+        mRecentRecyclerView.setLayoutManager(mRecentLayoutManager);
+//        mOldRecyclerView.setLayoutManager(mOldLayoutManager);
+
 
         mAdapter = new RecyclerView.Adapter<CustomViewHolder>() {
             @Override
@@ -86,11 +127,24 @@ public class NoticeBoard extends ActionBarActivity {
             }
 
         };
-        mRecyclerView.setAdapter(mAdapter);
+        mRecentRecyclerView.setAdapter(mAdapter);
+//        mOldRecyclerView.setAdapter(mAdapter);
+
+        if(mAdapter.getItemCount()==0){
+            emptyNotice.setVisibility(View.VISIBLE);
+            mRecentRecyclerView.setVisibility(View.GONE);
+
+        }
+        else{
+            emptyNotice.setVisibility(View.GONE);
+            mRecentRecyclerView.setVisibility(View.VISIBLE);
+
+        }
+
 
         SwipeDismissRecyclerViewTouchListener touchListener =
                 new SwipeDismissRecyclerViewTouchListener(
-                        mRecyclerView,
+                        mRecentRecyclerView,
                         new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
                             @Override
                             public boolean canDismiss(int position) {
@@ -105,13 +159,22 @@ public class NoticeBoard extends ActionBarActivity {
                                     mAdapter.notifyItemRemoved(position);
                                 }
                                 mAdapter.notifyDataSetChanged();
+                                Toast.makeText(NoticeBoard.this, "Notice Removed", Toast.LENGTH_SHORT).show();
+
+                                //If no notice available,
+
+                                if(mItems.size()==0){
+                                    mRecentRecyclerView.setVisibility(View.GONE);
+                                    emptyNotice.setVisibility(View.VISIBLE);
+                                }
+
                             }
                         });
-        mRecyclerView.setOnTouchListener(touchListener);
+        mRecentRecyclerView.setOnTouchListener(touchListener);
         // Setting this scroll listener is required to ensure that during ListView scrolling,
         // we don't look for swipes.
-        mRecyclerView.setOnScrollListener(touchListener.makeScrollListener());
-        mRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
+        mRecentRecyclerView.setOnScrollListener(touchListener.makeScrollListener());
+        mRecentRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this,
                 new OnItemClickListener() {
                     @Override
                     public void onItemClick(View view, int position) {
@@ -119,6 +182,7 @@ public class NoticeBoard extends ActionBarActivity {
                     }
                 }));
     }
+
 
     private class CustomViewHolder extends RecyclerView.ViewHolder {
 
@@ -162,5 +226,74 @@ public class NoticeBoard extends ActionBarActivity {
         @Override
         public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+
+        mMenu = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        SearchManager searchManager = (SearchManager)
+                getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchMenuItem = menu.findItem(R.id.search_notice);
+        android.support.v7.widget.SearchView searchView =
+                (android.support.v7.widget.SearchView) menu.findItem(R.id.search_notice).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(true);
+         searchView.setMaxWidth(3800);
+        SearchView.SearchAutoComplete theTextArea = (SearchView.SearchAutoComplete)searchView.findViewById(R.id.search_src_text);
+        theTextArea.setTextColor(Color.WHITE);//or any color that you want
+
+        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //searchView.setSubmitButtonEnabled(true);
+        //searchView.setOnQueryTextListener(this);
+
+        return true;
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_notice:
+                //onSearchRequested();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            MenuItem mi = mMenu.findItem(R.id.search_notice);
+            if(mi.isActionViewExpanded()){
+                mi.collapseActionView();
+            } else{
+                mi.expandActionView();
+            }
+        } else{
+            //onOptionsItemSelected(mMenu.findItem(R.id.search));
+        }
+        return super.onSearchRequested();
+    }
+
+
+    public boolean onQueryTextChange(String newText) {
+        Toast.makeText(NoticeBoard.this, "Query = " + newText, Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        Toast.makeText(NoticeBoard.this, "Query = " + query + " : submitted", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    protected boolean isAlwaysExpanded() {
+        return false;
     }
 }
