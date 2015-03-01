33c5c450789b10022a7b18915af68f7c29ac7a99
chiragshenoy <chiragshenoy@gmail.com>
2015-03-01 11:49:02 +0530

    Removed splash . Fixed About Us Page.

--- app/src/main/java/app/cloud9/com/cloud9/AboutPage.java
+++ app/src/main/java/app/cloud9/com/cloud9/AboutPage.java
@@ -3,10 +3,10 @@ package app.cloud9.com.cloud9;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
 import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
 
@@ -22,7 +22,7 @@ import me.alexrs.wavedrawable.WaveDrawable;
 /**
  * Created by chirag on 19/11/14.
  */
-public class AboutPage extends ActionBarActivity implements View.OnTouchListener, SpringListener {
+public class AboutPage extends ActionBarActivity {
 
     ImageView shub;
     ImageView sou;
@@ -35,83 +35,264 @@ public class AboutPage extends ActionBarActivity implements View.OnTouchListener
     private static double TENSION = 800;
     private static double DAMPER = 20; //friction
 
-    private SpringSystem mSpringSystem;
-    private Spring mSpring;
+    private SpringSystem mSpringSystem0;
+    private Spring mSpring0;
+
+    private SpringSystem mSpringSystem1;
+    private Spring mSpring1;
+
+    private SpringSystem mSpringSystem2;
+    private Spring mSpring2;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_about);
 
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.c9_toolbar);
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Meet the team");
+//        toolbar.setTitleTextColor(Color.WHITE);
+
         shub = (ImageView) findViewById(R.id.shub_img);
         sou = (ImageView) findViewById(R.id.sou_img);
         chi = (ImageView) findViewById(R.id.chir_img);
 
-        ShimmerTextView s = (ShimmerTextView) findViewById(R.id.shub_name);
+        final ShimmerTextView s = (ShimmerTextView) findViewById(R.id.shub_name);
+        final ShimmerTextView s1 = (ShimmerTextView) findViewById(R.id.sou_name);
+        final ShimmerTextView s2 = (ShimmerTextView) findViewById(R.id.chir_name);
 
-        Shimmer shimmer = new Shimmer();
-        shimmer.start(s);
-        waveDrawable = new WaveDrawable(Color.parseColor("#8e44ad"), 500);
+
+        final Shimmer shimmer = new Shimmer();
+        final Shimmer shimmer1 = new Shimmer();
+        final Shimmer shimmer2 = new Shimmer();
+
+        waveDrawable = new WaveDrawable(Color.parseColor("#8e44ad"), 75);
         shub.setBackgroundDrawable(waveDrawable);
-
         waveDrawable.setWaveInterpolator(new BounceInterpolator());
         waveDrawable.startAnimation();
-        waveDrawable1 = new WaveDrawable(Color.parseColor("#8e44ad"), 500);
+
+        waveDrawable1 = new WaveDrawable(Color.parseColor("#8e44ad"), 75);
         sou.setBackgroundDrawable(waveDrawable1);
         waveDrawable1.setWaveInterpolator(new AccelerateDecelerateInterpolator());
         waveDrawable1.startAnimation();
 
-        waveDrawable2 = new WaveDrawable(Color.parseColor("#8e44ad"), 500);
+        waveDrawable2 = new WaveDrawable(Color.parseColor("#8e44ad"), 75);
         chi.setBackgroundDrawable(waveDrawable2);
-        waveDrawable2.setWaveInterpolator(new AnticipateOvershootInterpolator());
+        waveDrawable2.setWaveInterpolator(new BounceInterpolator());
         waveDrawable2.startAnimation();
 
-        shub.setOnTouchListener(this);
-        mSpringSystem = SpringSystem.create();
+        //Shubham
+        mSpringSystem0 = SpringSystem.create();
+        mSpring0 = mSpringSystem0.createSpring();
+        mSpring0.addListener(new SpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                float value = (float) spring.getCurrentValue();
+                float scale = 1f - (value * 0.5f);
 
-        mSpring = mSpringSystem.createSpring();
-        mSpring.addListener(this);
+                shub.setScaleX(scale);
+                shub.setScaleY(scale);
+            }
+
+            @Override
+            public void onSpringAtRest(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringActivate(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringEndStateChange(Spring spring) {
+
+            }
+        });
 
         SpringConfig config = new SpringConfig(TENSION, DAMPER);
-        mSpring.setSpringConfig(config);
+        mSpring0.setSpringConfig(config);
+
+        shub.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                shimmer.start(s);
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mSpring0.setEndValue(1f);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        mSpring0.setEndValue(0f);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        //Shubham end
+
+        //SOu start
+        mSpringSystem1 = SpringSystem.create();
+        mSpring1 = mSpringSystem1.createSpring();
+        mSpring1.setSpringConfig(config);
+
+        sou.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                shimmer1.start(s1);
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mSpring1.setEndValue(1f);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        mSpring1.setEndValue(0f);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        mSpring1.addListener(new SpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                float value = (float) spring.getCurrentValue();
+                float scale = 1f - (value * 0.5f);
+
+                sou.setScaleX(scale);
+                sou.setScaleY(scale);
+            }
+
+            @Override
+            public void onSpringAtRest(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringActivate(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringEndStateChange(Spring spring) {
+
+            }
+        });
+
+        mSpring1.setSpringConfig(config);
+
+
+        //
+
+        //Chi start
+        mSpringSystem2 = SpringSystem.create();
+        mSpring2 = mSpringSystem2.createSpring();
+        mSpring2.setSpringConfig(config);
+
+        chi.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                shimmer2.start(s2);
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mSpring2.setEndValue(1f);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        mSpring2.setEndValue(0f);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        mSpring2.addListener(new SpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                float value = (float) spring.getCurrentValue();
+                float scale = 1f - (value * 0.5f);
+
+                chi.setScaleX(scale);
+                chi.setScaleY(scale);
+            }
+
+            @Override
+            public void onSpringAtRest(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringActivate(Spring spring) {
+
+            }
+
+            @Override
+            public void onSpringEndStateChange(Spring spring) {
+
+            }
+        });
+
+        mSpring2.setSpringConfig(config);
+
+
+        //chi end
+
+
+//        shub.setOnTouchListener(this);
+//        mSpringSystem = SpringSystem.create();
+//
+//        mSpring = mSpringSystem.createSpring();
+//        mSpring.addListener(this);
+//
+//        SpringConfig config = new SpringConfig(TENSION, DAMPER);
+//        mSpring.setSpringConfig(config);
+
 
     }
 
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mSpring.setEndValue(1f);
-                return true;
-            case MotionEvent.ACTION_UP:
-                mSpring.setEndValue(0f);
-                return true;
-        }
+//    @Override
+//    public boolean onTouch(View v, MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                mSpring.setEndValue(1f);
+//                return true;
+//            case MotionEvent.ACTION_UP:
+//                mSpring.setEndValue(0f);
+//                return true;
+//        }
+//
+//        return false;
+//    }
 
-        return false;
-    }
-
-    @Override
-    public void onSpringUpdate(Spring spring) {
-        float value = (float) spring.getCurrentValue();
-        float scale = 1f - (value * 0.5f);
-
-        shub.setScaleX(scale);
-        shub.setScaleY(scale);
-    }
-
-    @Override
-    public void onSpringAtRest(Spring spring) {
-
-    }
-
-    @Override
-    public void onSpringActivate(Spring spring) {
-
-    }
-
-    @Override
-    public void onSpringEndStateChange(Spring spring) {
-
-    }
+//    @Override
+//    public void onSpringUpdate(Spring spring) {
+//        float value = (float) spring.getCurrentValue();
+//        float scale = 1f - (value * 0.5f);
+//
+//        shub.setScaleX(scale);
+//        shub.setScaleY(scale);
+//    }
+//
+//    @Override
+//    public void onSpringAtRest(Spring spring) {
+//
+//    }
+//
+//    @Override
+//    public void onSpringActivate(Spring spring) {
+//
+//    }
+//
+//    @Override
+//    public void onSpringEndStateChange(Spring spring) {
+//
+//    }
 }
--- app/src/main/java/app/cloud9/com/cloud9/MainActivity.java
+++ app/src/main/java/app/cloud9/com/cloud9/MainActivity.java
@@ -5,48 +5,26 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.plus.Plus;
-import com.google.android.gms.plus.PlusClient;
-
-import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity implements NavigationDrawerCallbacks, GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener, View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
 
-    // nav drawer title
-    private CharSequence mDrawerTitle;
-
-    // used to store app title
-    private CharSequence mTitle;
-
-    // slide menu items
-    private String[] navMenuTitles;
-    private TypedArray navMenuIcons;
-
-    private ArrayList<NavDrawerItem> navDrawerItems;
-    private NavDrawerListAdapter adapter;
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    private PlusClient mPlusClient;
     private GoogleApiClient mGoogleApiClient;
 
     private static final int RC_SIGN_IN = 0;
@@ -62,12 +40,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
         email = bundle.getString("email");
         personName = bundle.getString("name");
 
-//        NavigationDrawerFragment navigationDrawerFragment = new NavigationDrawerFragment();
-//        Bundle args = new Bundle();
-//        args.putString("email", email);
-//        args.putString("name",personName);
-//        navigationDrawerFragment.setArguments(args);
-
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -77,15 +49,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
                 .build();
         mGoogleApiClient.connect();
 
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.c9_toolbar); //Appcompat support for a sexier action bar
-//        toolbar.setNavigationIcon(R.drawable.ic_drawer);
-//        toolbar.setTitleTextColor(Color.WHITE);
-//        setSupportActionBar(toolbar);
-//        toolbar.setTitle(R.string.app_name);
-//        toolbar.setNavigationIcon(R.drawable.ic_drawer);
-//
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.c9_toolbar);
         setSupportActionBar(toolbar);
@@ -103,71 +66,16 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
 
         mNavigationDrawerFragment.nav_usn.setText(usn);
 
-//        mTitle = mDrawerTitle = getTitle();
-//
-//
-//        // load slide menu items
-//        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
-//
-//        // nav drawer icons from resources
-//        navMenuIcons = getResources()
-//                .obtainTypedArray(R.array.nav_drawer_icons);
-//
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
-//
-//        navDrawerItems = new ArrayList<NavDrawerItem>();
-//
-//        // adding nav drawer items to array
-//        // Home
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
-//        // Find People
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
-//        // Photos
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
-//        // Communities, Will add a counter here
-//        navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1)));
-//
-//
-//        // Recycle the typed array
-//        navMenuIcons.recycle();
-//
-//        mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
-//
-//        // setting the nav drawer list adapter
-//        adapter = new NavDrawerListAdapter(getApplicationContext(),
-//                navDrawerItems);
-//        mDrawerList.setAdapter(adapter);
-////
-////        // enabling action bar app icon and behaving it as toggle button
-////        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFC107")));
-////        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-////        getSupportActionBar().setHomeButtonEnabled(true);
-//
-//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-//                R.drawable.ic_drawer, //nav menu toggle icon
-//                R.string.app_name, // nav drawer open - description for accessibility
-//                R.string.app_name // nav drawer close - description for accessibility
-//        ) {
-//            public void onDrawerClosed(View view) {
-//                getSupportActionBar().setTitle(mTitle);
-//                getSupportActionBar();
-//                // calling onPrepareOptionsMenu() to show action bar icons
-//                invalidateOptionsMenu();
-//            }
-//
-//            public void onDrawerOpened(View drawerView) {
-//                getSupportActionBar().setTitle(mDrawerTitle);
-//                // calling onPrepareOptionsMenu() to hide action bar icons
-//                invalidateOptionsMenu();
-//            }
-//        };
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
         if (savedInstanceState == null) {
             // on first time display view for first nav item
             displayView(0);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNavigationDrawerFragment.selectItem(0);
     }
 
     protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
@@ -240,53 +148,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
         else
             super.onBackPressed();
     }
-//
-//    /**
-//     * Slide menu item click listener
-//     */
-//    private class SlideMenuClickListener implements
-//            ListView.OnItemClickListener {
-//        @Override
-//        public void onItemClick(AdapterView<?> parent, View view, int position,
-//                                long id) {
-//            // display view for selected nav drawer item
-//            displayView(position);
-//        }
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.main_activity_actions, menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // toggle nav drawer on selecting action bar app icon/title
-//        if (mDrawerToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
-//        // Handle action bar actions click
-//        switch (item.getItemId()) {
-//            case R.id.action_refresh:
-//                displayView(0);
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
-//
-//    /**
-//     * Called when invalidateOptionsMenu() is triggered
-//     */
-//    @Override
-//    public boolean onPrepareOptionsMenu(Menu menu) {
-//        // if nav drawer is opened, hide the action items
-//        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-////        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-//        return super.onPrepareOptionsMenu(menu);
-//    }
 
     /**
      * Diplaying fragment view for selected nav drawer list item
@@ -301,21 +162,7 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
                 fragment = new HomePage();
                 fragment.setArguments(args);
                 break;
-//            case 1:
-//                fragment = new AboutPage();
-//                break;
-//            case 2:
-//                fragment = new PhotosFragment();
-//                break;
-//            case 3:
-//                fragment = new CommunityFragment();
-//                break;
-//            case 4:
-//                fragment = new PagesFragment();
-//                break;
-//            case 5:
-//                fragment = new WhatsHotFragment();
-//                break;
+
 
             default:
                 break;
@@ -326,16 +173,7 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
             fragmentManager.beginTransaction()
                     .replace(R.id.frame_container, fragment).commit();
         }
-//
-//            // update selected item and title, then close the drawer
-////            mDrawerList.setItemChecked(position, true);
-////            mDrawerList.setSelection(position);
-////            setTitle(navMenuTitles[position]);
-////            mDrawerLayout.closeDrawer(mDrawerList);
-//        } else {
-//            // error in creating fragment
-//            Log.e("MainActivity", "Error in creating fragment");
-//        }
+
     }
 
     @Override
@@ -357,32 +195,4 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
     public void onClick(View v) {
 
     }
-
-//    @Override
-//    public void setTitle(CharSequence title) {
-//        mTitle = title;
-//        getSupportActionBar().setTitle(mTitle);
-//    }
-
 }
-
-/**
- * When using the ActionBarDrawerToggle, you must call it during
- * onPostCreate() and onConfigurationChanged()...
- */
-
-//    @Override
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        // Sync the toggle state after onRestoreInstanceState has occurred.
-//        mDrawerToggle.syncState();
-//    }
-//
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        // Pass any configuration change to the drawer toggls
-//        mDrawerToggle.onConfigurationChanged(newConfig);
-//    }
-//
-//}
